f465d82bb2821d82b92c7eebc81a224cc967f05c
Dharma Putra <dharma66putra@gmail.com>
2024-01-15 21:57:54 +0700

    feat(db): configure connection to db

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,64 @@
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
+use config::Config;
+use db::DBClient;
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+
 mod config;
+mod db;
 mod models;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub env: Config,
+    pub db_client: DBClient,
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        // actix_web=info|debug
+        std::env::set_var("RUST_LOG", "actix_web=debug");
+    }
+
+    dotenv().ok();
+    env_logger::init();
+
+    let config = Config::init();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&config.database_url)
+        .await
+        .unwrap();
+
+    let db_client = DBClient::new(pool);
+    let app_state: AppState = AppState {
+        env: config.clone(),
+        db_client,
+    };
+
+    println!(
+        "{}",
+        format!("Server is running on http://localhost:{}", config.port)
+    );
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(app_state.clone()))
+            .wrap(Logger::default())
+            .service(health_checker_handler)
+    })
+    .bind(("0.0.0.0", config.port))?
+    .run()
+    .await?;
+
+    Ok(())
+}
+
+#[get("/api/healthchecker")]
+async fn health_checker_handler() -> impl Responder {
+    const MESSAGE: &str = "Complete Restful API in Rust";
+
+    HttpResponse::Ok().json(serde_json::json!({"status": "success", "message": MESSAGE}))
 }
