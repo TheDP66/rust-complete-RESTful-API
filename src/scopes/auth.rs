d28c44c480824a7f6d68998db4cb57b23322d347
dharma putra <dharma.putra@dwp.co.id>
2024-01-25 16:00:42 +0700

    add register admin

--- src/scopes/auth.rs
+++ src/scopes/auth.rs
@@ -52,6 +52,41 @@ pub async fn register(
     }
 }
 
+pub async fn registerAdmin(
+    app_state: web::Data<AppState>,
+    body: web::Json<RegisterUserDto>,
+) -> Result<HttpResponse, HttpError> {
+    body.validate()
+        .map_err(|e| HttpError::bad_request(e.to_string()))?;
+
+    let hashed_password =
+        password::hash(&body.password).map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    let result = app_state
+        .db_client
+        .save_admin_user(&body.name, &body.email, &hashed_password)
+        .await;
+
+    match result {
+        Ok(user) => Ok(HttpResponse::Created().json(UserResponseDto {
+            status: "success".to_string(),
+            data: UserData {
+                user: FilterUserDto::filter_user(&user),
+            },
+        })),
+        Err(sqlx::Error::Database(db_err)) => {
+            if db_err.is_unique_violation() {
+                Err(HttpError::unique_constraint_voilation(
+                    ErrorMessage::EmailExist,
+                ))
+            } else {
+                Err(HttpError::server_error(db_err.to_string()))
+            }
+        }
+        Err(e) => Err(HttpError::server_error(e.to_string())),
+    }
+}
+
 pub async fn login(
     app_state: web::Data<AppState>,
     body: web::Json<LoginUserDto>,
@@ -110,6 +145,12 @@ pub async fn logout() -> impl Responder {
 pub fn auth_scope() -> Scope {
     web::scope("/api/auth")
         .route("/register", web::post().to(register))
+        .route(
+            "/register/admin",
+            web::post()
+                .to(registerAdmin)
+                .wrap(RequireAuth::allowed_roles(vec![UserRole::Admin])),
+        )
         .route("/login", web::post().to(login))
         .route(
             "/logout",
